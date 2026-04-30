2c72465d49f37b9e26e51554528574e5f109aed6
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2026-04-30 23:35:53 +0900

    初期化中にJBSLのAPI取得が間に合わないとInvalid BSMLエラーになる問題の修正
    リーグ/リーダーボードのドロップダウンに読み込み中・データなしのプレースホルダーを導入した
    選択値の更新制御と有効/無効の切り替えを整理し、非同期更新中の誤操作を防ぐようにした
    候補再構築後の表示更新とインタラクティブ制御を見直し、初期化時のUI反映を安定化した

--- JBSLViewer/Views/LeaderboardPanelViewController.cs
+++ JBSLViewer/Views/LeaderboardPanelViewController.cs
@@ -1,7 +1,7 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -9,8 +9,8 @@ using HMUI;
 using Zenject;
 using SiraUtil.Zenject;
 using BeatSaberMarkupLanguage.Attributes;
-using BeatSaberMarkupLanguage.ViewControllers;
 using BeatSaberMarkupLanguage.Components.Settings;
+using BeatSaberMarkupLanguage.ViewControllers;
 using JBSLViewer.Configuration;
 using JBSLViewer.Models;
 
@@ -19,11 +19,23 @@ namespace JBSLViewer.Views
     [HotReload]
     public class LeaderboardPanelViewController : BSMLAutomaticViewController, IAsyncInitializable, ITickable
     {
+        private const string PlaceholderLeagueId = "-1";
+        private const string PlaceholderLeaderboardId = "-1";
+        private const string LoadingLeagueText = "Loading leagues...";
+        private const string LoadingLeaderboardText = "Loading leaderboards...";
+        private const string NoLeagueDataText = "No league data";
+        private const string NoLeaderboardDataText = "No leaderboard data";
+
         public bool _init = false;
         public float _currentCycleTime = 0f;
         public static SemaphoreSlim AllResetSemaphore = new SemaphoreSlim(1, 1);
         public static SemaphoreSlim SetLeaderboardSemaphore = new SemaphoreSlim(1, 1);
         public static SemaphoreSlim LeaderboardInfoSemaphore = new SemaphoreSlim(1, 1);
+
+        private bool _suppressLeagueSelectionChanged = false;
+        private bool _suppressLeaderboardSelectionChanged = false;
+        private bool _isLeagueLoading = true;
+        private bool _isLeaderboardLoading = true;
 
         [Inject]
         private readonly ActiveLeague _activeLeague;
@@ -57,99 +69,125 @@ namespace JBSLViewer.Views
                 this._leaderboardMainViewController.SetRecords();
             if (LeaderboardInfoSemaphore.CurrentCount == 0 || AllResetSemaphore.CurrentCount == 0 || SetLeaderboardSemaphore.CurrentCount == 0)
                 return;
-            if (!int.TryParse(this._jbslLeagueValue, out var leagueID))
+            if (!int.TryParse(this._jbslLeagueValue, out var leagueID) || leagueID == -1)
                 return;
-            var time = (this._latestUpdate._latest + TimeSpan.FromMinutes(PluginConfig.Instance.refreshInterval) - DateTime.Now).ToString(@"mm\:ss");
             if (this._leaderboard._leaderboards.ContainsKey(leagueID) && this._latestUpdate._latest > this._leaderboard._leaderboards[leagueID].jbslViewerGetTime)
                 _ = this.SetLeaderboardsAsync(this._leaderboardValue, true);
         }
 
         public async Task AllResetAsync()
         {
+            this._isLeagueLoading = true;
+            this._isLeaderboardLoading = true;
+            this.SetChoices(this.JBSLLeagueChoices, null, PlaceholderLeagueId);
+            this.SetChoices(this.LeaderboardChoices, null, PlaceholderLeaderboardId);
+            this.SetLeagueValueInternal(PlaceholderLeagueId);
+            this.SetLeaderboardValueInternal(PlaceholderLeaderboardId);
             if (this._init)
             {
-                this._leagueReloadButton.interactable = false;
-                this._reloadButton.interactable = false;
-                this._totalButton.interactable = false;
+                this.RefreshLeagueDropdown();
+                this.RefreshLeaderboardDropdown();
+                this.UpdateControlInteractivity();
             }
+
             await AllResetSemaphore.WaitAsync();
             try
             {
-                this.JBSLLeagueChoices.Clear();
-                if (this._init)
-                    this._jbslLeagueSetting.UpdateChoices();
                 this._leaderboardMainViewController.SetTitle("");
                 await this._latestUpdate.GetHeadlineAsync();
                 await this._activeLeague.GetActiveLeagueAsync();
                 if (this._activeLeague._leagues == null || this._activeLeague._leagues.Count <= 0)
                     return;
+
+                var leagueChoices = new List<object>();
                 foreach (var league in this._activeLeague._leagues)
-                    this.JBSLLeagueChoices.Add(league.id.ToString());
-                if (this._init)
-                    this._jbslLeagueSetting.UpdateChoices();
+                    leagueChoices.Add(league.id.ToString());
+                this.SetChoices(this.JBSLLeagueChoices, leagueChoices, PlaceholderLeagueId);
+
                 if (this._activeLeague.GetLeagueIndex(PluginConfig.Instance.selectLeagueID) == -1)
                     PluginConfig.Instance.selectLeagueID = this._activeLeague._leagues[0].id;
-                this.JBSLLeagueValue = PluginConfig.Instance.selectLeagueID.ToString();
+                this.SetLeagueValueInternal(PluginConfig.Instance.selectLeagueID.ToString());
                 if (this._init)
-                    this.NotifyPropertyChanged("JBSLLeagueValue");
+                    this.RefreshLeagueDropdown();
             }
             finally
             {
+                this._isLeagueLoading = false;
                 AllResetSemaphore.Release();
+                if (this._init)
+                {
+                    this.RefreshLeagueDropdown();
+                    this.RefreshLeaderboardDropdown();
+                    this.UpdateControlInteractivity();
+                }
             }
+
             await this.SetLeaderboardsAsync();
-            if (this._init)
-            {
-                this._leagueReloadButton.interactable = true;
-                this._reloadButton.interactable = true;
-                this._totalButton.interactable = true;
-            }
         }
 
         public async Task SetLeaderboardsAsync(string indexString = "0", bool reload = false)
         {
-            int leagueID;
+            var leagueID = -1;
+            string resolvedIndexString = PlaceholderLeaderboardId;
+            this._isLeaderboardLoading = true;
+            this.SetChoices(this.LeaderboardChoices, null, PlaceholderLeaderboardId);
+            this.SetLeaderboardValueInternal(PlaceholderLeaderboardId);
             if (this._init)
             {
-                this._leagueReloadButton.interactable = false;
-                this._reloadButton.interactable = false;
-                this._totalButton.interactable = false;
+                this.RefreshLeaderboardDropdown();
+                this.UpdateControlInteractivity();
             }
+
             await SetLeaderboardSemaphore.WaitAsync();
             try
             {
-                if (!int.TryParse(this._jbslLeagueValue, out leagueID))
+                if (!int.TryParse(this._jbslLeagueValue, out leagueID) || leagueID == -1)
                     return;
                 if (this._activeLeague.GetLeagueIndex(leagueID) == -1)
                     return;
+
                 PluginConfig.Instance.selectLeagueID = leagueID;
-                this.LeaderboardChoices.Clear();
-                if (this._init)
-                    this._leaderboardSetting.UpdateChoices();
                 this._leaderboardMainViewController.SetTitle("");
                 await this._leaderboard.GetLeaderboardAsync(leagueID, reload);
                 if (!this._leaderboard._leaderboards.ContainsKey(leagueID))
                     return;
+
                 var leaderboard = this._leaderboard._leaderboards[leagueID];
-                if (leaderboard == null)
+                if (leaderboard == null || leaderboard.maps == null)
                     return;
-                if (leaderboard.maps == null)
-                    return;
-                for (int i = 0; i <= leaderboard.maps.Count; i++)
-                    this.LeaderboardChoices.Add(i.ToString());
+
+                var leaderboardChoices = new List<object>();
+                for (var i = 0; i <= leaderboard.maps.Count; i++)
+                    leaderboardChoices.Add(i.ToString());
+                this.SetChoices(this.LeaderboardChoices, leaderboardChoices, PlaceholderLeaderboardId);
+                if (!this.LeaderboardChoices.Contains(indexString))
+                    indexString = this.LeaderboardChoices.Contains("0") ? "0" : this.LeaderboardChoices[0] as string;
+                resolvedIndexString = indexString;
             }
             finally
             {
+                this._isLeaderboardLoading = false;
                 SetLeaderboardSemaphore.Release();
+                if (this._init)
+                {
+                    this.RefreshLeaderboardDropdown();
+                    this.UpdateControlInteractivity();
+                }
             }
-            this.LeaderboardValue = indexString;
-            if (this._init)
+
+            if (!string.Equals(resolvedIndexString, PlaceholderLeaderboardId, StringComparison.Ordinal))
             {
-                this._leaderboardSetting.UpdateChoices();
-                this.NotifyPropertyChanged("LeaderboardValue");
+                this.SetLeaderboardValueInternal(resolvedIndexString);
+                if (this._init)
+                {
+                    this.RefreshLeaderboardDropdown();
+                    this.UpdateControlInteractivity();
+                }
             }
-            if (this._init)
-                this._totalButton.interactable = true;
+
+            if (leagueID == -1 || this.IsPlaceholderOnly(this.LeaderboardChoices, PlaceholderLeaderboardId))
+                return;
+
             await LeaderboardInfoSemaphore.WaitAsync();
             try
             {
@@ -158,23 +196,26 @@ namespace JBSLViewer.Views
             finally
             {
                 LeaderboardInfoSemaphore.Release();
-            }
-            if (this._init)
-            {
-                this._leagueReloadButton.interactable = true;
-                this._reloadButton.interactable = true;
+                if (this._init)
+                    this.UpdateControlInteractivity();
             }
         }
 
         public async Task LeagueReloadAsync()
         {
-            this.JBSLLeagueChoices.Clear();
-            this.LeaderboardChoices.Clear();
+            this._isLeagueLoading = true;
+            this._isLeaderboardLoading = true;
+            this.SetChoices(this.JBSLLeagueChoices, null, PlaceholderLeagueId);
+            this.SetChoices(this.LeaderboardChoices, null, PlaceholderLeaderboardId);
+            this.SetLeagueValueInternal(PlaceholderLeagueId);
+            this.SetLeaderboardValueInternal(PlaceholderLeaderboardId);
             if (this._init)
             {
-                this._leaderboardSetting.UpdateChoices();
-                this._jbslLeagueSetting.UpdateChoices();
+                this.RefreshLeagueDropdown();
+                this.RefreshLeaderboardDropdown();
+                this.UpdateControlInteractivity();
             }
+
             await this.AllResetAsync();
         }
 
@@ -195,6 +236,8 @@ namespace JBSLViewer.Views
         public void SetLeaderboard(string indexString)
         {
             if (AllResetSemaphore.CurrentCount == 0 || SetLeaderboardSemaphore.CurrentCount == 0)
+                return;
+            if (!this.LeaderboardChoices.Contains(indexString))
                 return;
             this.LeaderboardValue = indexString;
             if (this._init)
@@ -226,34 +269,47 @@ namespace JBSLViewer.Views
         private readonly Button _totalButton;
 
         [UIValue("JBSLLeagueChoices")]
-        public List<object> JBSLLeagueChoices { get; set; } = new List<object>();
+        public List<object> JBSLLeagueChoices { get; set; } = new List<object> { PlaceholderLeagueId };
 
         [UIValue("LeaderboardChoices")]
-        public List<object> LeaderboardChoices { get; set; } = new List<object>();
+        public List<object> LeaderboardChoices { get; set; } = new List<object> { PlaceholderLeaderboardId };
 
-        private string _jbslLeagueValue;
+        private string _jbslLeagueValue = PlaceholderLeagueId;
         [UIValue("JBSLLeagueValue")]
         public string JBSLLeagueValue
         {
             get => this._jbslLeagueValue;
             set
             {
+                value = string.IsNullOrEmpty(value) ? PlaceholderLeagueId : value;
+                if (string.Equals(this._jbslLeagueValue, value, StringComparison.Ordinal))
+                    return;
+
                 this._jbslLeagueValue = value;
+                if (this._suppressLeagueSelectionChanged || value == PlaceholderLeagueId)
+                    return;
                 if (LeaderboardInfoSemaphore.CurrentCount == 0 || AllResetSemaphore.CurrentCount == 0 || SetLeaderboardSemaphore.CurrentCount == 0)
                     return;
                 _ = this.SetLeaderboardsAsync();
             }
         }
 
-        private string _leaderboardValue;
+        private string _leaderboardValue = PlaceholderLeaderboardId;
         [UIValue("LeaderboardValue")]
         public string LeaderboardValue
         {
             get => this._leaderboardValue;
             set
             {
+                value = string.IsNullOrEmpty(value) ? PlaceholderLeaderboardId : value;
                 this._leaderboardValue = value;
-                this._leaderboardMainViewController.SetTitle(this.LeaderboardFormat(value));
+                if (this._suppressLeaderboardSelectionChanged)
+                    return;
+
+                if (value == PlaceholderLeaderboardId)
+                    this._leaderboardMainViewController.SetTitle("");
+                else
+                    this._leaderboardMainViewController.SetTitle(this.LeaderboardFormat(value));
             }
         }
 
@@ -282,8 +338,11 @@ namespace JBSLViewer.Views
         [UIAction("JBSLLeagueFormatter")]
         public string JBSLLeagueFormatter(string leagueID)
         {
+            if (string.Equals(leagueID, PlaceholderLeagueId, StringComparison.Ordinal))
+                return this._isLeagueLoading ? LoadingLeagueText : NoLeagueDataText;
             if (!int.TryParse(leagueID, out var id))
                 return "";
+
             var name = this._activeLeague.GetLeagueName(id);
             if (name == null)
                 return "!ERROR!";
@@ -292,10 +351,13 @@ namespace JBSLViewer.Views
 
         public string LeaderboardFormat(string indexString, int subString = int.MaxValue, bool totalText = false)
         {
+            if (string.Equals(indexString, PlaceholderLeaderboardId, StringComparison.Ordinal))
+                return "";
             if (!int.TryParse(indexString, out var index))
                 return "";
-            if (!int.TryParse(this._jbslLeagueValue, out var leagueID))
+            if (!int.TryParse(this._jbslLeagueValue, out var leagueID) || leagueID == -1)
                 return "";
+
             string result;
             if (index == 0)
             {
@@ -316,6 +378,8 @@ namespace JBSLViewer.Views
         [UIAction("LeaderboardFormatter")]
         public string LeaderboardFormatter(string indexString)
         {
+            if (string.Equals(indexString, PlaceholderLeaderboardId, StringComparison.Ordinal))
+                return this._isLeaderboardLoading ? LoadingLeaderboardText : NoLeaderboardDataText;
             return this.LeaderboardFormat(indexString, 20, true);
         }
 
@@ -331,9 +395,97 @@ namespace JBSLViewer.Views
             leaderboardLabel.fontSize = 2f;
             leaderboardLabel.enableWordWrapping = true;
             leaderboardLabel.overflowMode = TextOverflowModes.Ellipsis;
-            this.NotifyPropertyChanged("JBSLLeagueValue");
-            this.NotifyPropertyChanged("LeaderboardValue");
+            this.RefreshLeagueDropdown();
+            this.RefreshLeaderboardDropdown();
+            this.UpdateControlInteractivity();
             this._leaderboardMainViewController.SetTitle();
+        }
+
+        private void SetLeagueValueInternal(string value)
+        {
+            this._suppressLeagueSelectionChanged = true;
+            try
+            {
+                this._jbslLeagueValue = string.IsNullOrEmpty(value) ? PlaceholderLeagueId : value;
+            }
+            finally
+            {
+                this._suppressLeagueSelectionChanged = false;
+            }
+        }
+
+        private void SetLeaderboardValueInternal(string value)
+        {
+            this._suppressLeaderboardSelectionChanged = true;
+            try
+            {
+                this._leaderboardValue = string.IsNullOrEmpty(value) ? PlaceholderLeaderboardId : value;
+            }
+            finally
+            {
+                this._suppressLeaderboardSelectionChanged = false;
+            }
+
+            if (!this._init)
+                return;
+
+            if (this._leaderboardValue == PlaceholderLeaderboardId)
+                this._leaderboardMainViewController.SetTitle("");
+            else
+                this._leaderboardMainViewController.SetTitle(this.LeaderboardFormat(this._leaderboardValue));
+        }
+
+        private void SetChoices(List<object> choices, IEnumerable<object> values, string placeholderValue)
+        {
+            choices.Clear();
+            if (values != null)
+            {
+                foreach (var value in values)
+                    choices.Add(value);
+            }
+            if (choices.Count == 0)
+                choices.Add(placeholderValue);
+        }
+
+        private void RefreshLeagueDropdown()
+        {
+            this.RefreshDropdown(this._jbslLeagueSetting, this.JBSLLeagueChoices);
+        }
+
+        private void RefreshLeaderboardDropdown()
+        {
+            this.RefreshDropdown(this._leaderboardSetting, this.LeaderboardChoices);
+        }
+
+        private void RefreshDropdown(DropDownListSetting dropdown, List<object> options)
+        {
+            if (dropdown == null)
+                return;
+
+            dropdown.values = options;
+            dropdown.UpdateChoices();
+            dropdown.ReceiveValue();
+        }
+
+        private void UpdateControlInteractivity()
+        {
+            if (!this._init)
+                return;
+
+            var isBusy = LeaderboardInfoSemaphore.CurrentCount == 0 || AllResetSemaphore.CurrentCount == 0 || SetLeaderboardSemaphore.CurrentCount == 0;
+            var hasLeagueData = !this.IsPlaceholderOnly(this.JBSLLeagueChoices, PlaceholderLeagueId);
+            var hasLeaderboardData = !this.IsPlaceholderOnly(this.LeaderboardChoices, PlaceholderLeaderboardId);
+
+            this._jbslLeagueSetting.interactable = !isBusy && hasLeagueData;
+            this._leaderboardSetting.interactable = !isBusy && hasLeaderboardData;
+            this._leagueReloadButton.interactable = !isBusy;
+            this._reloadButton.interactable = !isBusy && hasLeagueData;
+            this._totalButton.interactable = !isBusy && this.LeaderboardChoices.Contains("0");
+        }
+
+        private bool IsPlaceholderOnly(List<object> choices, string placeholderValue)
+        {
+            return choices.Count == 1 && string.Equals(choices[0] as string, placeholderValue, StringComparison.Ordinal);
         }
     }
 }
